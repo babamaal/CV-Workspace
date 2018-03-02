364e097d9efa3da4caf9ccc69b4e75ebb72721f4
SANGUIN3-PC\Shyam <shyam@fabulyst.com>
2018-03-02 18:59:02 +0530

    bug fix

--- Keras_Wrapper/Neckline/main.py
+++ Keras_Wrapper/Neckline/main.py
@@ -9,7 +9,7 @@ class NeckCVObj(object):
 		self.model = get_model_from_meta('NECKLINE')
 		self.tags = get_tags_from_meta('NECKLINE')
 
-	def classify_for_image(img_pth):
+	def classify_for_image(self,img_pth):
 		## Input pre processing here : 
 		d = prepare_input_for_model(img_pth)
 		## Make predictions here : 
