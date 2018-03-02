7e2d924ae24f86af050360fd6a6e8d3ca4c52901
SANGUIN3-PC\Shyam <shyam@fabulyst.com>
2018-03-02 21:03:04 +0530

    minor

--- Pytorch_Wrapper/utils.py
+++ Pytorch_Wrapper/utils.py
@@ -1,12 +1,12 @@
 import zmq
 import json
-from cv_settings import settings
+from settings import cv_settings
 
 def make_pytorch_network_call(img, models=['all']):
     request_data = [{'img': img, 'attributes': models }]
     context = zmq.Context()
     socket = context.socket(zmq.DEALER)
-    socket.connect(settings.PYTORCH_SERVER)
+    socket.connect(cv_settings.PYTORCH_SERVER)
     socket.send_unicode(json.dumps(request_data))
     return socket.recv()
 
