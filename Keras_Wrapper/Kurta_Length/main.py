93fae0e064144335960c6f2fb795fc55b1ab0405
SANGUIN3-PC\Shyam <shyam@fabulyst.com>
2018-03-03 17:30:45 +0530

    minor

--- Keras_Wrapper/Kurta_Length/main.py
+++ Keras_Wrapper/Kurta_Length/main.py
@@ -14,9 +14,9 @@ class KLCVObj(object):
 
     def classify_for_image(self,img_pth):
         # Input pre processing here :
-        roi_flag = check_if_ankle_joint_present(img_pth)
-        print "ROI_FLAG" , roi_flag
-        if not roi_flag:
+        ankle_present = check_if_ankle_joint_present(img_pth)
+        print "ROI_FLAG" , ankle_present
+        if not ankle_present:
             predictions = 'Short Length'
             temp = {}
             return predictions, temp
